6945747b8b734bcab1da8094223cdedfcc97524d
Nayarit Gutierrez Nieto <165836550+142343@users.noreply.github.com>
2025-05-07 14:36:55 -0500

    Delete PROYECTO 3 Trimestre directory

--- PROYECTO 3 Trimestre/Boostrap/Modelo/Conexion.php
+++
@@ -1,9 +0,0 @@
-<?php  
-function Conectarse() {  
-    $conexion = mysqli_connect('localhost', 'root', '', 'paty_sport', '3307');  
-    if (!$conexion) {  
-        die("Conexión fallida: " . mysqli_connect_error());  
-    }  
-    return $conexion;  
-}  
-?>
